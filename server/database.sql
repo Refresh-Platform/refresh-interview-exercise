a687c9f14d3fa9bfe696ca0135751c981c2139b1
Andrew Sotzing <andrew.sotzing@gmail.com>
2026-04-29 16:19:21 -0400

    initial commit

---
+++ server/database.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE refresh_interview;
+CREATE TABLE todo(
+    todo_id SERIAL PRIMARY KEY,
+    description VARCHAR(30)
+);
---
+++ server/db.js
@@ -0,0 +1,17 @@
+const Pool = require("pg").Pool;
+
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_DATABASE:", process.env.DB_DATABASE); 
+
+const pool = new Pool({
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE
+});
+
+module.exports = pool;
